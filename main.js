642e3547af1b713955aafb335ed10a071c596dcd
Llyme <InfiniteX777@gmail.com>
2018-09-27 00:03:18 +0800

    Concept design.
    Note
    - These are yet to be official and are subject to change.
    
    New
    - Created `root.html`, `index.css`, `root.css`, `assets\js\*`, and `assets\img\*`.
    
    Changes
    - Renamed and moved `main.html` to `assets\html` as \index.html`.
    - Updated `package.json` to match github repo.
    - Created `test.bat` for testing purposes.
    
    Removed
    - `views` directory (use `assets` instead).

--- main.js
+++ main.js
@@ -1,38 +1,57 @@
-const electron = require('electron');
+const {app, BrowserWindow, session} = require("electron");
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+function init() {
+	// Clear up cache to prevent bloating.
+	session.defaultSession.clearStorageData();
+	session.defaultSession.clearCache(_ => _);
+	session.defaultSession.clearHostResolverCache();
 
-let mainWindow;
+	//Create a new window
+	let win = new BrowserWindow({
+		title: "A Very Nice Title" // You can set the title here as well.
+	});
 
-//Listen for app to be ready
-app.on('ready', ()=>{
-    
-    //Create a new window
-    mainWindow = new BrowserWindow({});
-    
-    //Load view into the window
-    mainWindow.loadURL(url.format({
-        pathname: './views/layouts/main.html',
-        protocol: 'file',
-        slashes: true,
-    }))
-    setMenu(menu);
-});
+	// This will disable the menu bar, though it gets disabled automatically
+	// in deployment anyway.
+	// win.setMenu(null);
 
-/*
-* Replaces the current menu bar of the app 
-* @param menu - an array of dropdown menus
-*/
+	//Load view into the window
+	win.loadFile("assets/html/root.html");
 
+	// We need the menu bar for testing. We'll use this later.
+	// setMenu(menu);
+
+	// This will unassign the object and be garbage-collected.
+	win.on("closed", () => win = null);
+}
+
+/**
+ * Replaces the current menu bar of the app 
+ * @param menu - an array of dropdown menus
+**/
 function setMenu(menuBar){
     Menu.setApplicationMenu(Menu.buildFromTemplate(menuBar));
 };
 
-//creates the menu bar which contains an array of dropdown menus
+// Creates the menu bar which contains an array of dropdown menus.
 const menu = [
     {
         label: 'Settings'
     },
 ];
+
+// Listen for app to be ready.
+app.on("ready", init);
+
+/* This will allow the app to shutdown properly.
+   Electron can make background processes and
+   can possibly prevent the app to close.
+*/
+app.on("window-all-closed", _ =>
+	process.platform !== "darwin" && app.quit()
+);
+
+// The 'ready' event for MacOS.
+app.on("activate", init);
